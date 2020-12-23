fab0bd430b3c6c2436bcf955656694dd59b03f6c
shabashab <shabashab@ukr.net>
2020-12-23 13:37:16 +0200

    Optimize grid rendering by removing some classes

--- GameOfLife/src/Graphics/GridRenderer.cpp
+++ GameOfLife/src/Graphics/GridRenderer.cpp
@@ -1,32 +1,28 @@
 ﻿#include "Graphics/GridRenderer.h"
-#include <chrono>
-
 
 void gol::GridRenderer::draw_vertical_lines(
-	olc::PixelGameEngine& engine) 
+	olc::PixelGameEngine& engine) const
 {
 	for(auto x = 0; x < engine.ScreenWidth(); x += this->cell_size_.x)
 	{
-		v_line_renderer_.setXPos(x, engine.ScreenHeight());
-		v_line_renderer_.Render(engine);
+		engine.DrawLine(x, 0, x, this->grid_size_.y);
 	}
 }
 
 void gol::GridRenderer::draw_horizontal_lines(
-	olc::PixelGameEngine& engine)	
+	olc::PixelGameEngine& engine) const
 {
 	for(auto y = 0; y < engine.ScreenHeight(); y += this->cell_size_.y)
 	{
-		h_line_renderer_.setYPos(y, engine.ScreenWidth());
-		h_line_renderer_.Render(engine);
+		engine.DrawLine(0, y, this->grid_size_.x, y);
 	}
 }
 
-gol::GridRenderer::GridRenderer(gol::Vector2 cell_size, Vector2 screen_size, olc::Pixel color = olc::GREY) :
-	v_line_renderer_(color, screen_size.y),
-	h_line_renderer_(color, screen_size.x)
+gol::GridRenderer::GridRenderer(gol::Vector2 cell_size, gol::Vector2 grid_size, olc::Pixel color)
 {
 	this->cell_size_ = cell_size;
+	this->grid_size_ = grid_size;
+	this->color_ = color;
 }
 
 void gol::GridRenderer::Render(olc::PixelGameEngine& engine)
